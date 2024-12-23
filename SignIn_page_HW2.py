aa4cfdd57b781e5531bdc95c028dceaa34eec86e
Uka O <uka.okeh@gmail.com>
2024-12-22 19:35:59 -0500

    Home Work 5

--- SignIn_page_HW2.py
+++ SignIn_page_HW2.py
@@ -1,7 +1,10 @@
+from mmap import ACCESS_COPY
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
 from time import sleep
 
 # get the path to the ChromeDriver executable
@@ -11,18 +14,20 @@ driver_path = ChromeDriverManager().install()
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
+driver.wait = WebDriverWait(driver, 10)
 
+ACC_NAV_SIGNIN = (By.XPATH, "//button[@data-test='accountNav-signIn']")
+SIGN_INTO_YOUR_ACC_TEXT = (By.XPATH, "//span[text()='Sign into your Target account']")
 # Open web page
 driver.get('https://www.target.com/')
 
 # Navigate to the right page
 driver.find_element(By.ID, 'account-sign-in').click()
-sleep(3)
-driver.find_element(By.XPATH, "//button[@data-test='accountNav-signIn']").click()
-sleep(3)
+driver.wait.until(EC.element_to_be_clickable(ACC_NAV_SIGNIN)).click()
+
 # Verification
 expected_result = 'Sign into your Target account'
-actual_result = driver.find_element(By.XPATH, "//span[text()='Sign into your Target account']").text
+actual_result = driver.wait.until(EC.presence_of_element_located(SIGN_INTO_YOUR_ACC_TEXT)).text
 assert expected_result == actual_result, f'Expected text {expected_result} not same as Actual text {actual_result}'
 print('Test case passed')
 
